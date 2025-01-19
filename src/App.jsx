2c3fd07ca2942e6658bd6d0249db84952cf27076
Asadbek <olimovasadbek828604@gmail.com>
2025-01-19 13:31:52 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import products from "./data/products.js"
+
+const App = () => {
+
+  return (
+    <div>
+      <ul className='product-list' >
+        {products.map(product => (
+          <li key={product.id} >
+            <div className={"product"}>
+              <div className="img-container" >
+                <img src={product.img} className='img'/>
+              </div>
+              <article>
+                <h3 className='product-title' >{product.title}</h3>
+                <p className='product-description' >{product.description}</p>
+                <span className='product-price' >Narxi: <b>{product.price}</b> so'm </span>
+              </article>
+              <div className="product-quantitys">
+                <p>blok: (<b>{product.blok}</b>) </p>
+                <p>dona: (<b>{product.quantity}</b>) </p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
