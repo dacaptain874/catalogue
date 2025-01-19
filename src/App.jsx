fd24209d093eab79dd7d278a92984856643b22d2
Asadbek <olimovasadbek828604@gmail.com>
2025-01-19 13:41:40 +0500

    Inint

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,26 @@ import products from "./data/products.js"
 const App = () => {
 
   return (
-    <div>
-      <ul className='product-list' >
-        {products.map(product => (
-          <li key={product.id} >
-            <div className={"product"}>
-              <div className="img-container" >
-                <img src={product.img} className='img'/>
-              </div>
-              <article>
-                <h3 className='product-title' >{product.title}</h3>
-                <p className='product-description' >{product.description}</p>
-                <span className='product-price' >Narxi: <b>{product.price}</b> so'm </span>
-              </article>
-              <div className="product-quantitys">
-                <p>blok: (<b>{product.blok}</b>) </p>
-                <p>dona: (<b>{product.quantity}</b>) </p>
-              </div>
+    <ul className='product-list' >
+      {products.map(product => (
+        <li key={product.id} >
+          <div className={"product"}>
+            <div className="img-container" >
+              <img src={product.img} className='img'/>
             </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+            <article>
+              <h3 className='product-title' >{product.title}</h3>
+              <p className='product-description' >{product.description}</p>
+              <span className='product-price' >Narxi: <b>{product.price}</b> so'm </span>
+            </article>
+            <div className="product-quantity">
+              <p>blok: (<b>{product.blok}</b>) </p>
+              <p>dona: (<b>{product.quantity}</b>) </p>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
 
