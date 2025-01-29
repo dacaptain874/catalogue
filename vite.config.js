aa6e54629c7a0f39d4d77a1719887c941281a870
Asadbek <olimovasadbek828604@gmail.com>
2025-01-29 22:31:21 +0500

    ini

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    legacy({
+      targets: ['ios 12'], // Ensure compatibility with iPad Mini 2
+      modernPolyfills: true,
+    }),
+  ],
+});
