1585c7b32066e9b813d451a0553a5cffa2aa2a86
Asadbek <olimovasadbek828604@gmail.com>
2025-01-30 20:02:28 +0500

    ini

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import products from "./data/products.js"
 import ProductList from './components/ProductList.jsx'
 
 const App = () => {
 
-  const [value, setValue] = useState("")
   const [filteredProducts, setFilteredProducts] = useState(products)
-
-  const searchValue = (e) => {
-    setValue(e.target.value)
-    const filtered =  products.filter(product => product.title.toLowerCase().includes(value.toLowerCase()))
-    setFilteredProducts(filtered)
-  }
 
   return (
     <div>
-      <div className='input-container'>
-        <input className="input" value={value} onChange={searchValue} type="text" placeholder='search...' />
-      </div>
       <ProductList products={filteredProducts} />
     </div>
   )
